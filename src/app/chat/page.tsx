024b9e51e17cec4cffd845bd4b034f520e6cf344
Souptik Sarkar <souptiksarkar4572@gmail.com>
2025-10-20 18:28:46 +0530

    add authentication using clerk

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -34,7 +34,7 @@ export default function RAGChatBot() {
 	};
 
 	return (
-		<div className="max-w-4xl mx-auto p-6 relative size-full h-[calc(100vh)]">
+		<div className="max-w-4xl mx-auto p-6 relative size-full h-[calc(100vh-4rem)]">
 			<div className="flex flex-col h-full">
 				<Conversation className="h-full">
 					<ConversationContent>
